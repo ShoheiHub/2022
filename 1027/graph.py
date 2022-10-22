4ba6d46bcddc1010289aa59644e1b74aa779aaaf
acguy <taniguchi.shohei.412@s.kyushu-u.ac.jp>
2022-10-23 02:44:14 +0900

    "None"

---
+++ 1027/graph.py
@@ -0,0 +1,44 @@
+#---------------------------------------------------------
+# graph.py
+#
+#   This is sample program and written by python
+#   Python can draw great picture.
+#
+#   graph.py <- input dat files.
+#
+#  author:T.Shohei
+#    data:2022.10.22
+#---------------------------------------------------------
+
+# python usually uses some convenience module
+import numpy as np # numerical module
+import matplotlib.pyplot as plt # graph module
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  lesson.1 : differentioal
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+file1="./lesson1.dat"
+with open(file1,mode='r') as f:
+    header = f.readline()
+    data1=f.readlines()
+
+N,=np.shape(data1)
+
+x=[]
+y=[]
+yp=[]
+
+for i in range(N):
+    x.append(float(data1[i].split()[0]))
+    y.append(float(data1[i].split()[1]))
+    yp.append(float(data1[i].split()[2]))
+
+plt.plot(x,y ,color="b",marker="o",markersize=2.0,linewidth=0.8,label="$sinx$")
+plt.plot(x,yp,color="r",marker="v",markersize=2.0,linewidth=0.8,label="$-cosx$")
+plt.legend()
+plt.show()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  lesson.2 : integration <- try
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
---
+++ 1027/main.py
@@ -0,0 +1,68 @@
+#---------------------------------------------------------
+# main.py
+#
+#   This is sample program and written by python
+#   Practice python coding and 
+#     differential and integration.
+#
+#  author:T.Shohei
+#    data:2022.10.22
+#---------------------------------------------------------
+
+# <- #(of character ) defines a comment.
+
+# python usually uses some convenience module
+import numpy as np # numerical module
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  lesson.1 : differentioal
+#    y = cosx -bibun-> dy/dx=-sinx
+#
+#  dy ~ y[i+1] - y[i-1]
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+N = 21
+
+xmin= 0
+xmax= 2*np.pi
+x = np.linspace(xmin,xmax,N) # min < x < max : size is N
+dx = (xmax-xmin)/(N-1)
+
+y = np.cos(x)
+
+# make empty array
+yp = np.zeros(N)
+
+for i in range(1,N-1):
+    yp[i] = ( y[i+1]-y[i-1] )/dx*0.5
+yp[0]   = (y[1]  -y[0]  )/dx
+yp[N-1] = (y[N-1]-y[N-2])/dx
+
+file1="./lesson1.dat"
+with open(file1,mode="w") as f:
+    f.write(f"%x y y_prime\n")
+    for i in range(N):
+        f.write(f"{x[i]:.3f} {y[i]:.3f} {yp[i]:.3f}\n")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  lesson.2 : integration
+#    y = cosx -sekibun-> Y=sinx
+#
+#  Y[i] = y[i]*dx
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def integration(N2):
+    x = np.linspace(xmin,xmax,N2)
+    dx = (xmax-xmin)/(N2-1)
+
+    y = np.cos(x)
+
+    Y = np.zeros(N2)
+    i=0
+    for i in range(1,N2):
+        Y[i] = Y[i-1]+y[i]*dx
+    return x, Y, y
+
+integration(100)
+
+# try to output dat file like above!!
